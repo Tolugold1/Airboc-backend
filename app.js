2dad4a6f1666959a8a5d32911d0b626fec9514ca
Tolulope <Steve@moilapp.com>
2025-02-14 00:07:16 -0800

    deployed to vercel

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cors({ origin: ["https://airbox-tgold.netlify.app/", "https://airboc-bac
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
