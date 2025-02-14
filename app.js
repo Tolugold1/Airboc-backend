65f0f473cf91fc6f46e40d1e7e9f718c3f12c995
Tolulope <Steve@moilapp.com>
2025-02-13 17:16:13 -0800

    update

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const passport = require("passport");
 // connect to the database.
 connectDB();
 
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:8000" ], credentials: true }));
+app.use(cors({ origin: ["https://airbox-tgold.netlify.app/", "https://airboc-backend.onrender.com" ], credentials: true }));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
